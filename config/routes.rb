8a8c57b2fd60c7c5b6ad81836bfffe6463c4aeb3
Albert Nguyen <Albert.la.nguyen@gmail.com>
2016-05-09 21:14:54 -0700

    minor changes to adventure model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:show, :index, :create, :destroy]
     resource :session, only: [:create, :destroy]
-    resources :adventures, only: [:create, :index, :destroy]
+    resources :adventures, only: [:show, :create, :index, :destroy]
   end
 end
